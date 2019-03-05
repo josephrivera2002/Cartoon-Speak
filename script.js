99b329172beb3b9fcfab518f5a2788aa261ccb29
josephrivera2002 <45007134+josephrivera2002@users.noreply.github.com>
2019-03-05 09:23:27 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$("#talk").click(function() {
+    console.log($("#speech").val());
+    // 1. Save the input value to a variable
+    var message=$("input").val();
+    // 2. Set the text of #speech-bubble to the saved value
+    
+    
+});
